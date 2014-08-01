b48b087dc9baa2d7a2a4aceac8afab3f377cb5fe
= <chitra.rani@qualtech-consultants.com>
2014-08-01 18:36:15 +0530

    correct production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,6 @@ TimesheetManagement::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
   
-  root :to => "home#index"
-
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
